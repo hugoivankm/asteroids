4dce77b11bf0dfb4e96a7d96d5724f6534f3a723
hugoivankm <hugoivank@gmail.com>
2024-09-14 13:13:04 -0600

    Add Player Sprite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from constants import *
+from player import Player
+
 
 
 def main():
@@ -7,6 +9,10 @@ def main():
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pg.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS)
 
     while (True):
         for event in pg.event.get():
@@ -15,6 +21,8 @@ def main():
 
         black = (0, 0, 0)
         screen.fill(black)
+        
+        player.draw(screen)
         
         pg.display.flip()
         dt = clock.tick(FPS) / 1000
---
+++ player.py
@@ -0,0 +1,18 @@
+from circleshape import CircleShape
+import pygame as pg
+
+class Player(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+        
+    def triangle(self):
+        forward = pg.Vector2(0, 1).rotate(self.rotation)
+        right = pg.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pg.draw.polygon(screen, "white", self.triangle(), 2)
